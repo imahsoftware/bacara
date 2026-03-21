d5ffbb53630a6882e6859f4bb517d8d78cd8dc5e
Fabian Fernandez <imahsoftware@gmail.com>
2026-03-21 08:25:54 -0500

    Actualizacion

--- app/controllers/jugadasdetalles_controller.rb
+++ app/controllers/jugadasdetalles_controller.rb
@@ -25,9 +25,9 @@ class JugadasdetallesController < ApplicationController
     if @detalle.save
       total = Jugadasdetalle.where(jugada_id: @jugada_id).count
 
-      #if total >= 5
-        ejecutar_prc_calculo_automatico(@jugada_id)
-      #end
+      if total >= 5
+        ejecutar_prc_calculo_automatico(@detalle.id)
+      end
 
       # Recargar todos los registros para actualizar la tabla
       @jugadasdetalles   = Jugadasdetalle.jugadas(@jugada_id)
