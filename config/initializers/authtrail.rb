e4cfc989c412e5089814f93a611bae20ab882c95
Juan Esteban Carrascal <juancudriz@gmail.com>
2026-06-09 21:49:58 -0500

    ***** TRADUCCIONES AL INGLES *****

--- config/initializers/authtrail.rb
+++ config/initializers/authtrail.rb
@@ -1,6 +1,6 @@
 AuthTrail.geocode = false  # desactivar geocoding externo (evita llamadas lentas a APIs)
 
-AuthTrail.transform_method = lambda do |data, request|
+AuthTrail.track_method = lambda do |data, request|
   # Capturar IP pública real, atravesando proxies/balanceadores
   # X-Forwarded-For puede tener múltiples IPs separadas por coma; la primera es la del cliente
   forwarded = request.env['HTTP_X_FORWARDED_FOR'].to_s.split(',').first.to_s.strip
